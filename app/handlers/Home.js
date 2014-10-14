92241ff204ec000961c970ae9e0452d850f73582
Ryan Florence <rpflorence@gmail.com>
2014-10-14 15:00:53 -0600

    added a little blurb

--- app/handlers/Home.js
+++ app/handlers/Home.js
@@ -6,7 +6,13 @@ var Home = module.exports = React.createClass({
   render: function() {
     return (
       <div>
-        <h2>WELCOME!</h2>
+        <h1 className="Heading Heading--alt">Welcome!</h1>
+        <div className="Content padBox">
+          <p>This app is rendered on the server and the client with React</p>
+          <p>You can actually turn off JavaScript and everything is still fully functional</p>
+          <p>Go ahead and create a new contact over on the left with and without JS enabled</p>
+          <p><a href="https://github.com/rackt/react-router-mega-demo">Code can be found here</a></p>
+        </div>
       </div>
     );
   }
