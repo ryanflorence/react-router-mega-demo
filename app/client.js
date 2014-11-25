8533c570748feab96a57fb040baf50de447e2d77
Ryan Florence <rpflorence@gmail.com>
2014-11-24 20:48:45 -0700

    sure ...

--- app/client.js
+++ app/client.js
@@ -2,9 +2,5 @@ require('./ENV');
 var React = require('react');
 var routes = require('./routes');
 var div = document.getElementById('app');
-React.renderComponent(routes, div, function() {
-  // clear it out so it never gets used again, otherwise getRouteProps will
-  // find this data even though it may have different params
-  window.ROUTER_PROPS = {};
-});
+React.renderComponent(routes, div);
 
