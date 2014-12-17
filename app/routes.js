1736008f07420f6e1367729af41af652d2adf07c
Ryan Florence <rpflorence@gmail.com>
2014-12-17 09:20:23 -0700

    capitalization woes

--- app/routes.js
+++ app/routes.js
@@ -1,23 +1,12 @@
 var React = require('react');
 var { DefaultRoute, Route } = require('react-router');
 
-//module.exports = (
-  //<Route name="root" path="/" handler={require('./handlers/Root')}>
-    //<DefaultRoute handler={require('./handlers/Home')} />
-    //<Route name="contact" path="contact/:id" handler={require('./handlers/Contact')} />
-    //<Route name="newContact" handler={require('./handlers/NewContact')} />
-    //<Route name="createContact" handler={require('./handlers/CreateContact')} />
-  //</Route>
-//);
-
-var Dumb = React.createClass({ render () { return null } });
-
 module.exports = (
   <Route name="root" path="/" handler={require('./handlers/Root')}>
     <DefaultRoute handler={require('./handlers/Home')} />
     <Route name="contact" path="contact/:id" handler={require('./handlers/Contact')} />
-    <Route name="newContact" handler={require('./handlers/newContact')} />
-    <Route name="createContact" handler={require('./handlers/createContact')} />
+    <Route name="newContact" handler={require('./handlers/NewContact')} />
+    <Route name="createContact" handler={require('./handlers/CreateContact')} />
   </Route>
 );
 
